9762acedc6f89928ce0c6dabcaa35048b2e12076
hans <771565119@qq.com>
2023-06-01 11:39:45 +0800

    dqwdq

--- javascript/index.ts
+++ javascript/index.ts
@@ -1,3 +1,92 @@
-namespace Test {
-  console.log('hello world')
+// type BackendUser = {
+//   // 用户 id 
+//   id: number
+//   // 用户 name
+//   name: string
+//   // 用户是否禁用：true 禁用，false 启用
+//   disabled: boolean
+// }
+
+// // 前端用户模型
+// type FrontendUser = {
+//   value: number
+//   label: string
+// }
+
+// let input = [{ id: 1, name: "jack", disabled: false }, { id: 2, name: "admin", disabled: true }]
+// let output = [{ value: 1, label: 'jack' }]
+
+// // 将后端用户列表转换成前端用户列表，只保留启用（disabled = false）的用户
+// function transform(users?: BackendUser[]): FrontendUser[] {
+//   if (!users) {
+//     return []
+//   }
+//   return users
+//     .filter(v => !v.disabled)
+//     .map(v => ({ value: v.id, label: v.name }))
+// }
+
+// console.log(transform(input))
+
+
+// getRawData 类型为 (...args: any[]) => Promise<any>
+
+// import { getRawData } from './services'
+
+// let getRawData: (...args: any[]) => Promise<any>
+
+// function getRawData(..._: any[]) {
+//   return new Promise<any>((resolve) => {
+//     setTimeout(() => {
+//       resolve(1)
+//     }, 1000 * 3)
+//   })
+// }
+
+// async function getData() {
+//   let t = 0 //当前时间
+//   let data: any;
+
+//   let timer = setInterval(() => {
+//     t += 0.1
+//     // console.log(t, data)
+//     //超过时间限制，且没有返回
+//     if (t > 10) {
+//       clearInterval(timer)
+//       if (!data) {
+//         throw new Error('time out')
+//       }
+//     }
+//   }, 100)
+//   data = await getRawData()
+// }
+// getData()
+
+
+function validate(input?: string | number) {
+  // todo
+  if (!input) {
+    throw new Error('请输入整数')
+  }
+
+  input = input.toString();
+
+  if(!/^\d+$/.test(input)){
+    throw new Error('请输入整数')
+  }
+}
+
+// 用例1
+try {
+  validate('12.9')
+} catch (e) {
+  console.log(e.message)
+  // 打印：只能输入整数
+}
+
+// 用例2
+try {
+  validate(12)
+} catch (e) {
+  console.log(e.message) // 不执行
 }
--- mysql/data_type.md
+++ mysql/data_type.md
@@ -1,33 +1,50 @@
 # 数据类型
 
 ## 字符串
-CHAR(n)          固定长度n
-VARCHAR(n)       可变长度，最大长度n
-TEXT             最大长度为65535的字符串
+```sql
+  CHAR(n)          固定长度n
+  VARCHAR(n)       可变长度，最大长度n
+  TEXT             最大长度为65535的字符串
+```
 
 ## 数字
-INT(n)
-	-2147483648 到 2147483647 常规。0 到 4294967295 无符号。
-  在括号中规定最大位数。n默认为11。
+在括号中规定最大位数。
+```sql
+  -- 整数
+  INT(n) 
+  -- -2147483648 ~ 2147483647 | 0 ~ 4294967295
+	-- n默认为11。
   
-BIGINT(n)
-  -9223372036854775808 到 9223372036854775807 常规。0 到 18446744073709551615 无符号。在括号中规定最大位数。n默认20
+  BIGINT(n)
+  -- -9223372036854775808 ~ 9223372036854775807
+  -- 0 ~ 18446744073709551615
+  -- n默认20
 
-FLOAT 带有小数点的小数字
-DOUBLE 带有小数点的大数字
+  -- 浮点数
+  FLOAT
+  DOUBLE
+
+  -- 布尔值
+  BIT(1) NOT NULL DEFAULT b'0';
+  -- (b'1', b'0')
+  -- select * from user where is_admin = 1;
+```
 
 
 ## 时间
-DATE()
+
+```sql
+  DATE()
   日期。格式：YYYY-MM-DD
   注释：支持的范围是从 '1000-01-01' 到 '9999-12-31'
 
-DATETIME()
+  DATETIME()
   *日期和时间的组合。格式：YYYY-MM-DD HH:MM:SS
   注释：支持的范围是从 '1000-01-01 00:00:00' 到 '9999-12-31 23:59:59'
 
-TIMESTAMP
+  TIMESTAMP
   时间戳
-  CURRENT_TIMESTAMP 当前时间
-  NOW() 可获取当前时间
+  SELECT CURRENT_TIMESTAMP() 获取当前时间
+  SELECT NOW() 可获取当前时间
   INSERT INTO user (username, create_time) VALUES ('hans', NOW());
+```
